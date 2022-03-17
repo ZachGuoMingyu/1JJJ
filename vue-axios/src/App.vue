bc96841dfb8a0703ba7f836aafd8b6fffeea1bb0
ZachGmyTea <guomy@briup.com>
2022-03-17 17:04:37 +0800

    third

--- vue-axios/src/App.vue
+++ vue-axios/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
     <router-view></router-view>
+    <van-tabbar v-model="active">
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/about" icon="search">关于</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
@@ -12,7 +16,12 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      active: 0,
+    };
+  },
 }
 </script>
 
